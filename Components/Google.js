a2b799dfc1ed7d206047293ae04febaf146cb92c
Jerry Fitzner <jfitzner1@gmail.com>
2023-06-04 22:40:50 -0400

    Added components

---
+++ Components/Google.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Google = () => {
+  return(
+    <View>
+      <Text>Google</Text>
+    </View>
+  )
+};
+
+export default Google;
---
+++ Components/Youtube.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Youtube = () => {
+  return(
+    <View>
+      <Text>Youtube</Text>
+    </View>
+  )
+};
+
+export default Youtube;
